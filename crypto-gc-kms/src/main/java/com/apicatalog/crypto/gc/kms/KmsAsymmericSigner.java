58170f692cc28898dc7ce8266dab8bf118b64f86
Filip <filip26@gmail.com>
2026-06-19 00:58:31 +0200

    Generalize getInstance method

--- crypto-gc-kms/src/main/java/com/apicatalog/crypto/gc/kms/KmsAsymmericSigner.java
+++ crypto-gc-kms/src/main/java/com/apicatalog/crypto/gc/kms/KmsAsymmericSigner.java
@@ -6,9 +6,9 @@ import java.security.SignatureException;
 
 import com.apicatalog.crypto.AsymmetricSigner;
 import com.google.cloud.kms.v1.AsymmetricSignRequest;
+import com.google.cloud.kms.v1.CryptoKeyVersion.CryptoKeyVersionAlgorithm;
 import com.google.cloud.kms.v1.Digest;
 import com.google.cloud.kms.v1.KeyManagementServiceClient;
-import com.google.cloud.kms.v1.PublicKey;
 import com.google.protobuf.ByteString;
 
 public class KmsAsymmericSigner implements AsymmetricSigner {
@@ -35,43 +35,44 @@ public class KmsAsymmericSigner implements AsymmetricSigner {
      * Creates a new {@link KmsAsymmericSigner} instance for the specified KMS algorithm
      */
     public static KmsAsymmericSigner getInstance(
-            PublicKey publicKey,
+            CryptoKeyVersionAlgorithm algorithm,
+            String resource,
             KeyManagementServiceClient kms) {
 
-        return switch (publicKey.getAlgorithm()) {
+        return switch (algorithm) {
         case EC_SIGN_P256_SHA256 -> new KmsAsymmericSigner(
                 KmsAsymmericSigner::ec256Sign,
                 kms,
-                publicKey.getName());
+                resource);
 
         case EC_SIGN_P384_SHA384 -> new KmsAsymmericSigner(
                 KmsAsymmericSigner::ec384Sign,
                 kms,
-                publicKey.getName());
+                resource);
 
         case EC_SIGN_ED25519 -> new KmsAsymmericSigner(
                 KmsAsymmericSigner::ed256Sign,
                 kms,
-                publicKey.getName());
+                resource);
 
         // PQ experiments
         case PQ_SIGN_SLH_DSA_SHA2_128S -> new KmsAsymmericSigner(
                 KmsAsymmericSigner::dsaSign,
                 kms,
-                publicKey.getName());
+                resource);
 
         case PQ_SIGN_ML_DSA_44 -> new KmsAsymmericSigner(
                 KmsAsymmericSigner::dsaSign,
                 kms,
-                publicKey.getName());
+                resource);
 
         case PQ_SIGN_ML_DSA_87 -> new KmsAsymmericSigner(
                 KmsAsymmericSigner::dsaSign,
                 kms,
-                publicKey.getName());
+                resource);
 
         default ->
-            throw new IllegalStateException("Unsupported KMS Key Algorithm [" + publicKey.getAlgorithm() + "]");
+            throw new IllegalStateException("Unsupported KMS Key Algorithm [" + algorithm + "]");
         };
     }
 
