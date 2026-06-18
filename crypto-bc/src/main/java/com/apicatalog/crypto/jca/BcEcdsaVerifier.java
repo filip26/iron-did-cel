79f26bf64e728c3bf0568668fc1a6546874a2be3
Filip <filip26@gmail.com>
2026-06-18 19:52:39 +0200

    Add iron-crypto-bc skeleton

---
+++ crypto-bc/src/main/java/com/apicatalog/crypto/jca/BcEcdsaVerifier.java
@@ -0,0 +1,107 @@
+package com.apicatalog.crypto.jca;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.ECPoint;
+import java.security.spec.ECPublicKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.ECPointUtil;
+import org.bouncycastle.jce.interfaces.ECPublicKey;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+import org.bouncycastle.jce.spec.ECNamedCurveSpec;
+
+public final class BcEcdsaVerifier {
+
+    private final String algorithm;
+    private final String curve;
+
+    public BcEcdsaVerifier(String algorithm, String curve) {
+        this.algorithm = algorithm;
+        this.curve = curve;
+    }
+
+    public static BcEcdsaVerifier getP256Instance() {
+        return new BcEcdsaVerifier(
+                "SHA256withECDSA",
+                "secp256r1");
+    }
+
+    public static BcEcdsaVerifier getP384Instance() {
+        return new BcEcdsaVerifier(
+                "SHA384withECDSA",
+                "secp384r1");
+    }
+
+    public boolean verify(final byte[] publicKey, final byte[] data, final byte[] signature)
+            throws SignatureException, InvalidKeyException {
+
+        try {
+            var verifier = Signature.getInstance(algorithm);
+
+            verifier.initVerify(getPublicKeyFromBytes(publicKey));
+
+            verifier.update(data);
+
+            return verifier.verify(toDerSignature(signature));
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+
+        } catch (InvalidKeySpecException e) {
+            throw new InvalidKeyException(e);
+
+        } catch (IOException e) {
+            throw new SignatureException(e);
+        }
+    }
+
+    private PublicKey getPublicKeyFromBytes(final byte[] pubKey)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        var keyFactory = KeyFactory.getInstance("EC", new BouncyCastleProvider());
+
+        final ECNamedCurveParameterSpec spec = ECNamedCurveTable.getParameterSpec(curve);
+        final ECNamedCurveSpec params = new ECNamedCurveSpec(curve, spec.getCurve(), spec.getG(), spec.getN(),
+                spec.getH());
+        final ECPoint point = ECPointUtil.decodePoint(params.getCurve(), pubKey);
+        final ECPublicKeySpec pubKeySpec = new ECPublicKeySpec(point, params);
+        return (ECPublicKey) keyFactory.generatePublic(pubKeySpec);
+    }
+
+    private static byte[] toDerSignature(final byte[] signature) throws IOException {
+
+        if (signature == null) {
+            throw new IllegalArgumentException("'signature' parameter must not be null.");
+        }
+        if (signature.length != 64 && signature.length != 96) {
+            throw new IllegalArgumentException("'signature' must be exactly 64 or 96 bytes long.");
+        }
+
+        final byte[] rBytes = Arrays.copyOfRange(signature, 0, signature.length / 2);
+        final byte[] sBytes = Arrays.copyOfRange(signature, signature.length / 2, signature.length);
+
+        final BigInteger r = new BigInteger(1, rBytes);
+        final BigInteger s = new BigInteger(1, sBytes);
+
+        final DERSequence sequence = new DERSequence(new ASN1Encodable[] {
+                new ASN1Integer(r),
+                new ASN1Integer(s)
+        });
+
+        return sequence.getEncoded();
+    }
+
+}
---
+++ crypto-bc/src/main/java/com/apicatalog/crypto/jca/BcEdDsaVerifier.java
@@ -0,0 +1,113 @@
+package com.apicatalog.crypto.jca;
+
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.EdECPoint;
+import java.security.spec.EdECPublicKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.security.spec.NamedParameterSpec;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public final class BcEdDsaVerifier {
+
+    private final String algorithm;
+
+    public BcEdDsaVerifier(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public static BcEdDsaVerifier getInstance() {
+        return new BcEdDsaVerifier("Ed25519");
+    }
+
+    public boolean verify(final byte[] publicKey, final byte[] data, final byte[] signature)
+            throws SignatureException, InvalidKeyException {
+
+        try {
+            var verifier = Signature.getInstance(algorithm);
+
+            verifier.initVerify(getPublicKeyFromBytes(publicKey));
+            verifier.update(data);
+
+            return verifier.verify(signature);
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+
+        } catch (InvalidKeySpecException | InvalidKeyException | InvalidParameterSpecException e) {
+            throw new InvalidKeyException(e);
+
+        }
+    }
+
+//    @Override
+//    public byte[] sign(byte[] privateKey, byte[] data) throws SigningError {
+//
+//        try {
+//            java.security.Signature suite = java.security.Signature.getInstance(type);
+//
+//            suite.initSign(getPrivateKey(privateKey));
+//            suite.update(data);
+//
+//            return suite.sign();
+//
+//        } catch (InvalidParameterSpecException | InvalidKeySpecException | InvalidKeyException
+//                | NoSuchAlgorithmException | SignatureException e) {
+//            throw new SigningError(SigningError.Code.Internal, e);
+//        }
+//    }
+
+    private PublicKey getPublicKeyFromBytes(final byte[] publicKey)
+            throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidParameterSpecException {
+
+        var keyFactory = KeyFactory.getInstance("Ed25519", new BouncyCastleProvider());
+        
+        // determine if x was odd.
+        boolean xisodd = false;
+
+        int lastbyteInt = publicKey[publicKey.length - 1];
+
+        if ((lastbyteInt & 255) >> 7 == 1) {
+            xisodd = true;
+        }
+
+        // make public key copy
+        byte[] key = new byte[publicKey.length];
+        System.arraycopy(publicKey, 0, key, 0, key.length);
+
+        // make sure most significant bit will be 0 - after reversing.
+        key[key.length - 1] &= 127;
+
+        key = reverse(key);
+
+        BigInteger y = new BigInteger(1, key);
+
+        NamedParameterSpec paramSpec = new NamedParameterSpec(keyFactory.getAlgorithm());
+        EdECPoint ep = new EdECPoint(xisodd, y);
+        EdECPublicKeySpec pubSpec = new EdECPublicKeySpec(paramSpec, ep);
+        return keyFactory.generatePublic(pubSpec);
+    }
+
+//    private PrivateKey getPrivateKey(byte[] privateKey)
+//            throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidParameterSpecException {
+//
+//        NamedParameterSpec paramSpec = new NamedParameterSpec(keyFactory.getAlgorithm());
+//        EdECPrivateKeySpec spec = new EdECPrivateKeySpec(paramSpec, privateKey);
+//        return keyFactory.generatePrivate(spec);
+//    }
+//
+    private final static byte[] reverse(byte[] data) {
+        final byte[] reversed = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            reversed[data.length - i - 1] = data[i];
+        }
+        return reversed;
+    }
+}
